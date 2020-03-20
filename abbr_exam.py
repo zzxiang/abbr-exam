d2b7060d60fb5c94cf2343d67b27d9c176c2ab5c
Zhixiang Zhu <zzxiang21cn@hotmail.com>
2020-03-20 22:04:14 +0900

    First commit, initial version

---
+++ abbr_exam.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import csv
+import random
+import sys
+from PyQt5 import QtWidgets
+
+from window import Ui_MainWindow as ExamWindow
+
+
+file = open('abbr.csv')
+reader = csv.reader(file)
+data_table = [row for row in reader]
+file.close()
+print('Read', len(data_table), 'abbreviations.')
+
+
+app = QtWidgets.QApplication(sys.argv)
+main_window = QtWidgets.QMainWindow()
+ui = ExamWindow()
+ui.setupUi(main_window)
+ui.menubar.hide()
+
+data_row_picked = None
+
+
+def pick_abbr():
+    index = random.randint(0, len(data_table) - 1)
+    global data_row_picked
+    data_row_picked = data_table[index]
+    ui.abbr.setText(data_row_picked[0])
+    ui.lineEdit.clear()
+    ui.lineEdit.returnPressed.connect(submit)
+    ui.msg.setText('<p>Please input the full name.</p><p>Press enter to submit answer.</p>')
+
+
+def submit():
+    ui.lineEdit.returnPressed.disconnect(submit)
+    if ui.lineEdit.text().strip().lower() == data_row_picked[1].lower():
+        ui.msg.setText('<p style="color: green">Correct!</p><p>Press enter to go next.')
+    else:
+        msg = """
+        <p><span style="color: red">Wrong!</span> Correct answer is:</p>
+        <p style="color: orange">%s</p>
+        <p>Press enter to go next.</p>
+        """ % data_row_picked[1]
+        ui.msg.setText(msg)
+    ui.lineEdit.returnPressed.connect(pick_abbr)
+
+
+pick_abbr()
+
+main_window.show()
+sys.exit(app.exec_())
---
+++ window.py
@@ -0,0 +1,99 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'window.ui'
+#
+# Created by: PyQt5 UI code generator 5.14.1
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(500, 300)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(MainWindow.sizePolicy().hasHeightForWidth())
+        MainWindow.setSizePolicy(sizePolicy)
+        MainWindow.setMinimumSize(QtCore.QSize(500, 300))
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.abbr = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(2)
+        sizePolicy.setHeightForWidth(self.abbr.sizePolicy().hasHeightForWidth())
+        self.abbr.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(35)
+        font.setBold(True)
+        font.setWeight(75)
+        self.abbr.setFont(font)
+        self.abbr.setAlignment(QtCore.Qt.AlignCenter)
+        self.abbr.setObjectName("abbr")
+        self.verticalLayout.addWidget(self.abbr)
+        self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(2)
+        sizePolicy.setHeightForWidth(self.lineEdit.sizePolicy().hasHeightForWidth())
+        self.lineEdit.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(24)
+        self.lineEdit.setFont(font)
+        self.lineEdit.setText("Plan, Do, Check, Act")
+        self.lineEdit.setAlignment(QtCore.Qt.AlignCenter)
+        self.lineEdit.setObjectName("lineEdit")
+        self.verticalLayout.addWidget(self.lineEdit)
+        self.msg = QtWidgets.QLabel(self.centralwidget)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(3)
+        sizePolicy.setHeightForWidth(self.msg.sizePolicy().hasHeightForWidth())
+        self.msg.setSizePolicy(sizePolicy)
+        font = QtGui.QFont()
+        font.setPointSize(22)
+        self.msg.setFont(font)
+        self.msg.setTextFormat(QtCore.Qt.RichText)
+        self.msg.setAlignment(QtCore.Qt.AlignCenter)
+        self.msg.setObjectName("msg")
+        self.verticalLayout.addWidget(self.msg)
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QtWidgets.QMenuBar(MainWindow)
+        self.menubar.setGeometry(QtCore.QRect(0, 0, 500, 22))
+        self.menubar.setNativeMenuBar(False)
+        self.menubar.setObjectName("menubar")
+        self.menuFile = QtWidgets.QMenu(self.menubar)
+        self.menuFile.setObjectName("menuFile")
+        MainWindow.setMenuBar(self.menubar)
+        self.actionImportCSV = QtWidgets.QAction(MainWindow)
+        self.actionImportCSV.setObjectName("actionImportCSV")
+        self.menuFile.addAction(self.actionImportCSV)
+        self.menubar.addAction(self.menuFile.menuAction())
+
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "Abbreviation Exam"))
+        self.abbr.setText(_translate("MainWindow", "TextLabel"))
+        self.msg.setText(_translate("MainWindow", "<html><head/><body><p>Please input the full name.</p><p>Press enter to submit.</p></body></html>"))
+        self.menuFile.setTitle(_translate("MainWindow", "File"))
+        self.actionImportCSV.setText(_translate("MainWindow", "Import CSV"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
